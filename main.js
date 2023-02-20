e4652e188ecb5942732c50b9bbbdc4b8cb38204f
Raine <rainepark1@gmail.com>
2023-02-20 18:41:55 +1100

    Clean up timetable sync output

--- main.js
+++ main.js
@@ -46,6 +46,10 @@ function getdatafortoday(){
   newday = new Date();
   day = newday.getDay();
   currentsec = newday.getHours()*3600+newday.getMinutes()*60+newday.getSeconds();
+  
+  // Change day variable for debugging 
+  //day=1
+
   //get data for today's day
   if (day == 1){
     daydata = belltimes.Monday;
@@ -231,13 +235,29 @@ function showtimetable(){
 			}
 		}
 		for (let x=1; x < Object.keys(daytimetable).length+1; x++){
-			cleanclassname=daytimetable[x][0].split(": ")[1].split(" ");
+			cleanclassname=daytimetable[x][0].replace(/.$/, '').split(": ")[1].split(" ");
 			if (cleanclassname[0]=="Yr" && isNumeric(cleanclassname[1])){
 				cleanclassname.shift();
 				cleanclassname.shift();
 			}
-			cleanclassname=cleanclassname.join(" ");
-			document.getElementById("tableid").rows[tableindexes[x]].cells[0].innerHTML=document.getElementById("tableid").rows[tableindexes[x]].cells[0].innerHTML+"<br>"+"<a class='timetableinfo'>"+cleanclassname+" in "+daytimetable[x][1]+"</a>";
+			cleanclassname=cleanclassname.join(" ")+" ("+daytimetable[x][0].split(": ")[0]+")";
+
+			if (daytimetable[x][1]!=""){
+				classlocation=" in " + daytimetable[x][1]
+			}
+			else{
+				classlocation=""
+			}
+
+			if (cleanclassname.split(" ")[0]=="Study"){
+				cleanclassname="Study Period"
+				classlocation=""
+			}
+			if (cleanclassname.split(" ")[3] == "Developmen"){
+				cleanclassname=cleanclassname.replace("Developmen","Development")
+			}
+
+			document.getElementById("tableid").rows[tableindexes[x]].cells[0].innerHTML=document.getElementById("tableid").rows[tableindexes[x]].cells[0].innerHTML+"<br>"+"<a class='timetableinfo'>"+cleanclassname+classlocation+"</a>";
 			document.getElementById("tableid").rows[tableindexes[x]].cells[0].outerHTML=document.getElementById("tableid").rows[tableindexes[x]].cells[0].outerHTML.replace(" class=\"notimetable\"","");
 			//document.getElementById("tableid").rows[tableindexes[x]].cells[1].innerHTML=document.getElementById("tableid").rows[tableindexes[x]].cells[1].innerHTML+"<br><a class='timetableinfo'>Room: "+daytimetable[x][1]+" || Teacher: "+daytimetable[x][2]+"</a>";
 			//document.getElementById("tableid").rows[tableindexes[x]].cells[1].innerHTML=document.getElementById("tableid").rows[tableindexes[x]].cells[1].innerHTML+"<br><a class='timetableinfo'>Room: "+daytimetable[x][1]+"</a>";
