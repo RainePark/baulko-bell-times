e10573f65da37bbe3f1d073c807a9f9540cc27b4
Raine Park <rainepark1@gmail.com>
2025-02-12 00:05:59 +1100

    Update to 2025 bell times

--- belltimes.js
+++ belltimes.js
@@ -2,10 +2,10 @@ var belltimes = {
   "Monday":{
     "period":[
       "Start",
-      "Go to P1 Class - NO ROLL CALL",
+      "Roll Call",
       "Period 1",
       "Period 2",
-      "Assembly (in Gym)",
+      "Assembly",
       "Recess",
       "Period 3",
       "Period 4",
@@ -18,38 +18,38 @@ var belltimes = {
       "End"
     ],
     "times":[
-      518,
-      520,
-      520,
-      557,
-      557,
-      594,
-      594,
-      619,
-      619,
-      639,
-      639,
-      676,
-      676,
-      713,
-      713,
-      718,
-      718,
-      755,
-      755,
-      792,
-      792,
-      832,
-      832,
-      869,
-      869,
-      906
+      518, // Roll Call [Start]
+      524, // Roll Call [End]
+      524, // Period 1 [Start]
+      563, // Period 1 [End]
+      563, // Period 2 [Start]
+      600, // Period 2 [End]
+      600, // Assembly [Start]
+      624, // Assembly [End]
+      624, // Recess [Start]
+      644, // Recess [End]
+      643, // Period 3 [Start]
+      680, // Period 3 [End]
+      680, // Period 4 [Start]
+      717, // Period 4 [End]
+      717, // Break [Start]
+      722, // Break [End]
+      722, // Period 5 [Start]
+      759, // Period 5 [End]
+      759, // Period 6 [Start]
+      796, // Period 6 [End]
+      796, // Lunch [Start]
+      832, // Lunch [End]
+      832, // Period 7 [Start]
+      869, // Period 7 [End]
+      869, // Period 8 [Start]
+      906  // Period 8 [End]
     ]
   },
   "Tuesday":{
     "period":[
       "Start",
-      "Go to P1 Class - NO ROLL CALL",
+      "Roll Call",
       "Period 1",
       "Period 2",
       "Recess",
@@ -64,36 +64,36 @@ var belltimes = {
       "End"
     ],
     "times":[
-      518,
-      520,
-      520,
-      561,
-      561,
-      601,
-      601,
-      621,
-      621,
-      661,
-      661,
-      701,
-      701,
-      706,
-      706,
-      746,
-      746,
-      786,
-      786,
-      826,
-      826,
-      866,
-      866,
-      906
+      518, // Roll Call [Start]
+      524, // Roll Call [End]
+      524, // Period 1 [Start]
+      566, // Period 1 [End]
+      566, // Period 2 [Start]
+      606, // Period 2 [End]
+      606, // Recess [Start]
+      625, // Recess [End]
+      625, // Period 3 [Start]
+      665, // Period 3 [End]
+      665, // Period 4 [Start]
+      705, // Period 4 [End]
+      705, // Break [Start]
+      710, // Break [End]
+      710, // Period 5 [Start]
+      750, // Period 5 [End]
+      750, // Period 6 [Start]
+      790, // Period 6 [End]
+      790, // Lunch [Start]
+      826, // Lunch [End]
+      826, // Period 7 [Start]
+      866, // Period 7 [End]
+      866, // Period 8 [Start]
+      906  // Period 8 [End]
     ]
   },
   "Wednesday":{
     "period":[
       "Start",
-      "Go to P1 Class - NO ROLL CALL",
+      "Roll Call",
       "Period 1",
       "Period 2",
       "Recess",
@@ -108,36 +108,36 @@ var belltimes = {
       "End"
     ],
     "times":[
-      518,
-      520,
-      520,
-      558,
-      558,
-      596,
-      596,
-      611,
-      611,
-      649,
-      649,
-      687,
-      687,
-      725,
-      725,
-      750,
-      750,
-      870,
-      751,
-      791,
-      791,
-      830,
-      830,
-      870
+      518, // Roll Call [Start]
+      524, // Roll Call [End]
+      524, // Period 1 [Start]
+      563, // Period 1 [End]
+      563, // Period 2 [Start]
+      600, // Period 2 [End]
+      600, // Recess [Start]
+      615, // Recess [End]
+      615, // Period 3 [Start]
+      652, // Period 3 [End]
+      652, // Period 4 [Start]
+      689, // Period 4 [End]
+      689, // Period 5 [Start]
+      726, // Period 5 [End]
+      726, // Lunch [Start]
+      759, // Lunch [End]
+      759, // Sport [Start]
+      870, // Sport [End]
+      759, // Period 6 [Start]
+      796, // Period 6 [End]
+      796, // Period 7 [Start]
+      833, // Period 7 [End]
+      833, // Period 8 [Start]
+      870  // Period 8 [End]
     ]
   },
   "Thursday":{
     "period":[
       "Start",
-      "Go to P1 Class - NO ROLL CALL",
+      "Roll Call",
       "Period 1",
       "Period 2",
       "Recess",
@@ -152,36 +152,36 @@ var belltimes = {
       "End"
     ],
     "times":[
-      518,
-      520,
-      520,
-      561,
-      561,
-      601,
-      601,
-      621,
-      621,
-      661,
-      661,
-      701,
-      701,
-      706,
-      706,
-      746,
-      746,
-      786,
-      786,
-      826,
-      826,
-      866,
-      866,
-      906
+      518, // Roll Call [Start]
+      524, // Roll Call [End]
+      524, // Period 1 [Start]
+      566, // Period 1 [End]
+      566, // Period 2 [Start]
+      606, // Period 2 [End]
+      606, // Recess [Start]
+      625, // Recess [End]
+      625, // Period 3 [Start]
+      665, // Period 3 [End]
+      665, // Period 4 [Start]
+      705, // Period 4 [End]
+      705, // Break [Start]
+      710, // Break [End]
+      710, // Period 5 [Start]
+      750, // Period 5 [End]
+      750, // Period 6 [Start]
+      790, // Period 6 [End]
+      790, // Lunch [Start]
+      826, // Lunch [End]
+      826, // Period 7 [Start]
+      866, // Period 7 [End]
+      866, // Period 8 [Start]
+      906  // Period 8 [End]
     ]
   },
   "Friday":{
     "period":[
       "Start",
-      "Go to P1 Class - NO ROLL CALL",
+      "Roll Call",
       "Period 1",
       "Period 2",
       "Recess",
@@ -196,31 +196,30 @@ var belltimes = {
       "End"
     ],
     "times":[
-      518,
-      520,
-      520,
-      561,
-      561,
-      601,
-      601,
-      626,
-      626,
-      666,
-      666,
-      706,
-      706,
-      711,
-      711,
-      751,
-      751,
-      791,
-      791,
-      826,
-      826,
-      866,
-      866,
-      906
+      518, // Roll Call [Start]
+      524, // Roll Call [End]
+      524, // Period 1 [Start]
+      566, // Period 1 [End]
+      566, // Period 2 [Start]
+      605, // Period 2 [End]
+      605, // Recess [Start]
+      630, // Recess [End]
+      630, // Period 3 [Start]
+      669, // Period 3 [End]
+      669, // Period 4 [Start]
+      708, // Period 4 [End]
+      708, // Break [Start]
+      713, // Break [End]
+      713, // Period 5 [Start]
+      752, // Period 5 [End]
+      752, // Period 6 [Start]
+      791, // Period 6 [End]
+      791, // Lunch [Start]
+      827, // Lunch [End]
+      827, // Period 7 [Start]
+      867, // Period 7 [End]
+      867, // Period 8 [Start]
+      906  // Period 8 [End]
     ]
   }
-
 };
